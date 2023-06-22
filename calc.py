9e978059d8e69b8d3c68f0e2df45976e1303030d
devonLikesJava <devhulse04@icloud.com>
2023-06-22 10:11:35 -0400

    kindaFinishedcalc

--- calc.py
+++ calc.py
@@ -29,24 +29,113 @@ while initInput>0 and initInput<7:
     if initInput == 1:
         num1, num2 = eval(input("Insert the 2 digits you would like to add!"))
         print(add(num1, num2))
-        again = input("Do you want another calculation? (y/n)")
-        if again == y:
-            
-        break
+        oof = 0
+        if oof == 0:
+            again = input("Do you want another calculation? (y/n)")
+            if again == "y":
+                initInput = eval(input("\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
+                oof = 1
+            elif again == "n":
+                oof = 1
+                break
+            else:
+                print("That is not a valid response, please try again.")
+                break
+     
+     
+        
     elif initInput == 2:
-        print(subtract())
-        break
+        num1, num2 = eval(input("Insert the 2 digits you would like to subtract!"))
+        print(subtract(num1, num2))
+        oof = 0
+        if oof == 0:
+            again = input("Do you want another calculation? (y/n)")
+            if again == "y":
+                initInput = eval(input("\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
+                oof = 1
+            elif again == "n":
+                oof = 1
+                break
+            else:
+                print("That is not a valid response, please try again.")
+                break
+        
+        
+        
     elif initInput == 3:
-        print(multi())
-        break
+        num1, num2 = eval(input("Insert the 2 digits you would like to multiply!"))
+        print(multi(num1, num2))
+        oof = 0
+        if oof == 0:
+            again = input("Do you want another calculation? (y/n)")
+            if again == "y":
+                initInput = eval(input("\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
+                oof = 1
+            elif again == "n":
+                oof = 1
+                break
+            else:
+                print("That is not a valid response, please try again.")
+                break        
+        
+        
+        
+        
     elif initInput == 4:
-        print(divide())
-        break
+        num1, num2 = eval(input("Insert the 2 digits you would like to divide!"))
+        print(divide(num1, num2))
+        oof = 0
+        if oof == 0:
+            again = input("Do you want another calculation? (y/n)")
+            if again == "y":
+                initInput = eval(input("\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
+                oof = 1
+            elif again == "n":
+                oof = 1
+                break
+            else:
+                print("That is not a valid response, please try again.")
+                break
+        
+        
+        
+        
+       
     elif initInput == 5:
-        print(sqrt())
-        break
+        num1 = eval(input("Insert the digit you would like to find the square root of!"))
+        print(sqrt(num1))
+        oof = 0
+        if oof == 0:
+            again = input("Do you want another calculation? (y/n)")
+            if again == "y":
+                initInput = eval(input("\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
+                oof = 1
+            elif again == "n":
+                oof = 1
+                break
+            else:
+                print("That is not a valid response, please try again.")
+                break
+      
+      
+      
+      
+      
     elif initInput == 6:
-        print(expo())
-        break
+        num1, num2 = eval(input("Insert the 2 digits first one being the primary number 2nd one being the exponent."))
+        print(expo(num1, num2))
+        oof = 0
+        if oof == 0:
+            again = input("Do you want another calculation? (y/n)")
+            if again == "y":
+                initInput = eval(input("\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
+                oof = 1
+            elif again == "n":
+                oof = 1
+                break
+            else:
+                print("That is not a valid response, please try again.")
+                break
+      
 else:
     print("That is not a valid input")
