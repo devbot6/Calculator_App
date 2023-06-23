2efbc31bd48a2b844ca5c0f52efddb348fc15d6a
devonLikesJava <devhulse04@icloud.com>
2023-06-23 08:08:32 -0400

    comp calc

--- calc.py
+++ calc.py
@@ -1,8 +1,24 @@
 import math
 
-#Menu
-initInput = eval(input("This is a calculator app!\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent"))
 
+
+some_bool = True
+while (some_bool):
+    #Menu
+    initInput = input("This is a calculator app!\nChoose what you want to formulate from below: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square Root\n6. Exponent")
+    
+    try:
+        initInput = eval(initInput)
+    except:
+        print("That is a string please input an integer.")
+        some_bool = True
+    if str != type(initInput):   
+        some_bool = False
+    
+
+    
+
+    
 
 #Functions
 
@@ -24,8 +40,11 @@ def sqrt(num1):
 def expo(num1, num2):
     return num1**num2
 
+
+
+
 #Logic
-while initInput>0 and initInput<7:
+while ((initInput>0) and (initInput<7)):
     if initInput == 1:
         num1, num2 = eval(input("Insert the 2 digits you would like to add!"))
         print(add(num1, num2))
